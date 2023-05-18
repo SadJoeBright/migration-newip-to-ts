d06b895c5f9b95235db610c283dcc5d8068173f5
SadJoeBright <sadjoebright.gmail.com>
2023-05-19 02:54:53 +0300

    the placement of mines after the first move has been implemented

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,8 +1,9 @@
 let numberOfRows = 10;
 let numberOfColumns = 10;
-let numberOfMines = 10;
+let numberOfMines = 20;
+let mines = [];
 
-function createMatrix(rows, columns, mines) {
+function createMatrix(rows, columns, mines, x ,y) {
   const matrix = [];
   for (let i = 0; i < rows; i += 1) {
     matrix[i] = [];
@@ -14,15 +15,15 @@ function createMatrix(rows, columns, mines) {
   while (minesCount < mines) {
     const randomRow = Math.floor(Math.random() * rows);
     const randomCol = Math.floor(Math.random() * columns);
-    if (matrix[randomRow][randomCol] !== '*') {
-      matrix[randomRow][randomCol] = '*';
+    if (matrix[randomRow][randomCol] !== '💣'  && (randomRow !== x || randomCol !== y)) {
+      matrix[randomRow][randomCol] = '💣';
       minesCount += 1;
     }
   }
-  let res = matrix.map(item => item.map(el => '*'));
+  let res = matrix.map(item => item.map(el => '💣'));
 	for (i = 0; i < matrix.length; i++) {		
 		for (j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] !== '*') {
+      if (matrix[i][j] !== '💣') {
         let count = '';
         if (matrix[i-1] && matrix[i-1][j-1]) count = +count + 1;
         if (matrix[i-1] && matrix[i-1][j]) count = +count + 1;
@@ -39,33 +40,49 @@ function createMatrix(rows, columns, mines) {
 	return res;  
 }
 
-let matrix = createMatrix(numberOfRows, numberOfColumns, numberOfMines);
+
 
 const mineField = document.createElement('div');
 mineField.classList.add('mine-field');
 document.body.prepend(mineField); 
 
-for (let i = 0; i < matrix.length; i += 1 ) {
+for (let i = 0; i < numberOfColumns; i += 1 ) {
   const row = document.createElement('div');
   row.classList.add('row');
-  for (let j = 0; j < matrix[i].length; j += 1) {
+  for (let j = 0; j < numberOfRows; j += 1) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.innerHTML = matrix[i][j];
     row.append(cell);
   }
   mineField.append(row) 
 }
 
+function setMines(event) {
+  const rowIndex = [...document.querySelectorAll('.row')].indexOf(event.target.parentNode);
+  const columnIndex = [...event.target.parentNode.querySelectorAll('.cell')].indexOf(event.target);
+  document.querySelectorAll('.cell').forEach(cell => cell.removeEventListener('click', setMines));
+  mines =  createMatrix(numberOfRows, numberOfColumns, numberOfMines, rowIndex, columnIndex);
+  for (let i = 0; i < numberOfColumns; i += 1 ) {
+    for (let j = 0; j < numberOfRows; j += 1) {
+      document.querySelectorAll('.row')[i].querySelectorAll('.cell')[j].innerHTML = mines[i][j];
+    }
+  }
+  document.querySelectorAll('.cell').forEach(cell => cell.removeEventListener('click', setMines));
+}
+
+
+
 function openCell(event) {
   const rowIndex = [...document.querySelectorAll('.row')].indexOf(event.target.parentNode);
   const columnIndex = [...event.target.parentNode.querySelectorAll('.cell')].indexOf(event.target);
-  if (event.target.innerHTML === '*') {
+  if (event.target.innerHTML === '💣') {
     event.target.style.backgroundColor = 'red';
     document.querySelectorAll('.cell').forEach((cell) => {
-      if (cell.innerHTML === '*') cell.classList.add('cell_opened')
-    })
-  }  
+      cell.removeEventListener('click', openCell);
+      cell.removeEventListener('contextmenu', setFlag);
+      if (cell.innerHTML === '💣' && !cell.classList.contains('cell_flaged')) cell.classList.add('cell_opened')
+    });
+  }   
   
   function openEmptyCells(row, col) {
     const rows = document.querySelectorAll('.row'); 
@@ -78,6 +95,8 @@ function openCell(event) {
   
       if (!cell.classList.contains('cell_opened')) {
         cell.classList.add('cell_opened');
+        cell.removeEventListener('click', openCell);
+        cell.removeEventListener('contextmenu', setFlag)  
   
         if (cell.innerHTML === '') {
           openEmptyCells(row - 1, col - 1);
@@ -90,16 +109,34 @@ function openCell(event) {
           openEmptyCells(row + 1, col + 1);
         } else if (!isNaN(cell.innerHTML)) {
           cell.classList.add('cell_opened');
+          cell.removeEventListener('click', openCell);
+          cell.removeEventListener('contextmenu', setFlag)  
         }
       }
     }
   }
   
   openEmptyCells(rowIndex, columnIndex);
-  event.target.classList.add('cell_opened');  
+  event.target.classList.add('cell_opened');
+  event.target.removeEventListener('click', openCell);
+  event.target.removeEventListener('contextmenu', setFlag)  
+}
+
+
+function setFlag(event) {
+  event.preventDefault();
+  event.target.classList.toggle('cell_flaged');
+  if (event.target.classList.contains('cell_flaged')) {
+    event.target.removeEventListener('click', openCell);
+    // event.target.innerHTML = content;
+  } else {
+    event.target.addEventListener('click', openCell);
+  }
 }
   
-  // 🚩💣
+  // 🚩
   
-
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', setMines));
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', openCell));
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('contextmenu', setFlag));
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('contextmenu', (event) => event.preventDefault()));
