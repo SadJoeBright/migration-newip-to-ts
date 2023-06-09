37a617982b3ce2cf36c92d2f51cdfd9fcaf59010
SadJoeBright <sadjoebright.gmail.com>
2023-06-09 21:13:46 +0300

    feat: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'bb7027a1d0dc48e488ca04d0ca500a70', // получите свой ключ https://newsapi.org/
         });
     }
 }
