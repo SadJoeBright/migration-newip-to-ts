fb918b62d04dbdaccf0269d10dc74a73c7d4eb94
SadJoeBright <sadjoebright.gmail.com>
2023-06-12 23:40:48 +0300

    feat: migrate file sources.js to sources.ts

---
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,36 @@
+import './sources.css';
+import { ApiSource } from '../../../types';
+
+class Sources<T extends ApiSource> {
+    draw(data: T[]): void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+
+        if (sourceItemTemp) {
+            data.forEach((item: T) => {
+                const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment | null;
+
+                if (sourceClone) {
+                    const itemNameElement: Element | null = sourceClone.querySelector('.source__item-name');
+                    if (itemNameElement) {
+                        itemNameElement.textContent = item.name;
+                    }
+
+                    const sourceItemElement: Element | null = sourceClone.querySelector('.source__item');
+                    if (sourceItemElement) {
+                        sourceItemElement.setAttribute('data-source-id', item.id);
+                    }
+
+                    fragment.append(sourceClone);
+                }
+            });
+        }
+
+        const sourcesContainer: Element | null = document.querySelector('.sources');
+        if (sourcesContainer) {
+            sourcesContainer.append(fragment);
+        }
+    }
+}
+
+export default Sources;
