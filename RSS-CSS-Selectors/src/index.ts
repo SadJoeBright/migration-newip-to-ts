eea2a2f4bc4ed235f2fd8833d1bc69d0b4d97812
SadJoeBright <sadjoebright.gmail.com>
2023-07-04 03:30:50 +0300

    chore: set alert

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,8 +1,7 @@
 /* eslint-disable no-alert */
 import App from './components/app/app';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const app = new App();
-// app.createView();
+app.createView();
 // eslint-disable-next-line max-len
-// alert('Дорогой Reviwer! Eсли у тебя есть такая возможность, пожалуйста отложи свою проверку до вечера 6.03. Буду безмерно благодарен)) Мой discord: @SadJoeBright#6933; telegram: @sadjoebright');
+alert('Дорогой Reviwer! Eсли у тебя есть такая возможность, пожалуйста отложи свою проверку до вечера 6.03. Буду безмерно благодарен)) Мой discord: @SadJoeBright#6933; telegram: @sadjoebright');
